8c7d0d9a00759ef48c628e06d58a62830d9c36ed
spences10 <spences10apps@gmail.com>
2024-03-24 14:08:49 +0000

    refactor: :recycle: add link form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,8 @@
 import { lucia } from '$lib/server/auth';
+import { turso_client } from '$lib/server/db';
 import { fail, redirect } from '@sveltejs/kit';
 
-import type { Actions, PageServerLoad } from './$types';
+import type { Action, Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
@@ -12,17 +13,42 @@ export const load: PageServerLoad = async (event) => {
 	};
 };
 
-export const actions: Actions = {
-	default: async (event) => {
-		if (!event.locals.session) {
-			return fail(401);
-		}
-		await lucia.invalidateSession(event.locals.session.id);
-		const sessionCookie = lucia.createBlankSessionCookie();
-		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: '.',
-			...sessionCookie.attributes
+const sign_out: Action = async (event) => {
+	if (!event.locals.session) {
+		return fail(401);
+	}
+	await lucia.invalidateSession(event.locals.session.id);
+	const session_cookie = lucia.createBlankSessionCookie();
+	event.cookies.set(session_cookie.name, session_cookie.value, {
+		path: '.',
+		...session_cookie.attributes
+	});
+	return redirect(302, '/login');
+};
+
+const add_link: Action = async ({ request, locals }) => {
+	const client = turso_client();
+	const form_data = Object.fromEntries(await request.formData());
+	const { url, title } = form_data;
+
+	// Insert the link into the database
+	try {
+		await client.execute({
+			sql: `INSERT INTO link (url, title, created_at, user_id) VALUES (?, ?, ?, ?)`,
+			args: [
+				url as string,
+				title as string,
+				Date.now(),
+				locals.user?.id ?? 'default_user_id'
+			]
 		});
-		return redirect(302, '/login');
+		return {
+			success: true
+		};
+	} catch (error) {
+		console.error('Failed to insert link:', error);
+		return fail(500, { message: 'Failed to insert link' });
 	}
 };
+
+export const actions: Actions = { sign_out, add_link };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,21 @@
 	import { enhance } from '$app/forms';
 
 	let { data } = $props();
+
+	let url = $state('https://scottspence.com');
+	let title = $state('thing');
 </script>
 
 <h1>Hi, {data.user.username}!</h1>
 <p>Your user ID is {data.user.id}.</p>
-<form method="post" use:enhance>
+<form method="POST" action="?/sign_out" use:enhance>
 	<button>Sign out</button>
+</form>
+
+<form method="POST" action="?/add_link" use:enhance>
+	<label for="url">URL:</label>
+	<input name="url" bind:value={url} type="url" required />
+	<label for="title">Title:</label>
+	<input name="title" bind:value={title} type="text" required />
+	<button type="submit">Add Link</button>
 </form>
