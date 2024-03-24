5317b30fab33e63aaa49774a969f69f7e52efac1
spences10 <spences10apps@gmail.com>
2024-03-24 12:40:05 +0000

    refactor: :recycle: autoincrement

--- src/lib/server/db/schema.sql
+++ src/lib/server/db/schema.sql
@@ -1,40 +1,39 @@
 CREATE TABLE
   IF NOT EXISTS user (
-    id TEXT NOT NULL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
   );
 
 CREATE TABLE
   IF NOT EXISTS session (
-    id TEXT NOT NULL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     expires_at INTEGER NOT NULL,
-    user_id TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
   );
 
 CREATE TABLE
   IF NOT EXISTS link (
-    id TEXT NOT NULL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     url TEXT NOT NULL,
     title TEXT NOT NULL,
     created_at INTEGER NOT NULL,
-    user_id TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id)
   );
 
 CREATE TABLE
   IF NOT EXISTS tag (
-    id TEXT NOT NULL PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE
   );
 
 CREATE TABLE
   IF NOT EXISTS link_tag (
-    link_id TEXT NOT NULL,
-    tag_id TEXT NOT NULL,
+    link_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
     PRIMARY KEY (link_id, tag_id),
     FOREIGN KEY (link_id) REFERENCES link (id),
     FOREIGN KEY (tag_id) REFERENCES tag (id)
   );
-
